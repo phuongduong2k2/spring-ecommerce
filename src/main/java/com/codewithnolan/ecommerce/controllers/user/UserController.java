94f8be17af0558cfdb45c69a6159dd4aebae763a
phuongduong2k2 <dqphuong2k2@gmail.com>
2026-05-10 20:51:41 +0700

    update creat user

--- src/main/java/com/codewithnolan/ecommerce/controllers/user/UserController.java
+++ src/main/java/com/codewithnolan/ecommerce/controllers/user/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 @Validated
 @RestController
